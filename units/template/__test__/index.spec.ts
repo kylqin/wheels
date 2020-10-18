b8f4c6d76c0491b835743e98012b8537c77f5ba6
kylqin <kylqin@outlook.com>
2020-10-18 17:49:27 +0800

    impl: template

--- units/template/__test__/index.spec.ts
+++ units/template/__test__/index.spec.ts
@@ -1,7 +1,45 @@
-import { add } from '../'
+import { compile } from '..';
 
-describe('add', () => {
-  it('compute the sum of two numbers', () => {
-    expect(add(2, 3)).toBe(5)
-  })
-})
+describe('模板编译', () => {
+  it('{{}} 表达式', () => {
+    const output = compile('<b>{{ name }}</b>')({ name: 'tom' });
+    expect(output).toBe(`<b>tom</b>`);
+  });
+
+  it('{{}} toUpperCase 表达式', () => {
+    const output = compile('<b>{{ name.toUpperCase() }}</b>')({ name: 'tom' });
+    expect(output).toBe(`<b>TOM</b>`);
+  });
+
+  it('{{}} +连接', () => {
+    const output = compile(`<b>{{ '[' + name + ']' }}</b>`)({ name: 'tom' });
+    expect(output).toBe(`<b>[tom]</b>`);
+  });
+
+  it('forEach 遍历', () => {
+    const output = compile(
+      `{%arr.forEach(item => {%}
+    <li>{{item}}</li>
+{%})%}`
+    )({
+      arr: ['aaa', 'bbb'],
+    });
+    expect(output).toBe(
+      `
+    <li>aaa</li>
+
+    <li>bbb</li>
+`
+    );
+  });
+
+  it('if 表达式', () => {
+    const output = compile(`{% if(isShow) { %} <b>{{ name }}</b> {% } %}`)({
+      isShow: true,
+      name: 'tom',
+    });
+    expect(output).toBe(
+      ` <b>tom</b> `
+    );
+  });
+});
--- units/template/index.ts
+++ units/template/index.ts
@@ -1,1 +1,30 @@
-export const add = (a: number, b: number) => a + b
+export const compile = (template: string) => {
+  // 1. {{ exp }} 变为 ${exp}
+  template = template.replace(/\{\{([^}]+)\}\}/g, (_:any, exp: string) => {
+    exp = exp.trim()
+    return '${' + exp + '}'
+  })
+
+
+  // 2. 使用 with(context) 语法, 构造函数体
+  let head = 'let str = "";\n with(obj){\n str+=`'
+
+  // 3. {% exp %} 变为 `
+  // exp
+  // str+=`
+  // {% exp-start %} 和 {% exp-end %} 是成对出现的，它们构成一个完整的 循环、判断等表达式。
+  // 在处理这种表达式时，使用 str += ` 的方式连接后面的内容
+  template = template.replace(/\{\%([^%]+)\%\}/g, (_: any, exp: string) => {
+    return '`\n' + exp + '\nstr+=`'
+  })
+
+  // 4. 尾部
+  let tail = '`}\n return str;'
+
+  // console.log(`==========render=========`)
+  // console.log(head + template + tail);
+
+  const render = new Function('obj', head + template + tail) as (context: { [key: string]: any }) => string
+  // return ctx => { console.log('"' + render(ctx) + '"'); return render(ctx) }
+  return render
+}
